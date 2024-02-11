4d5c3b2e59cc5a66a38693d95f9a74bca2fc0c7b
kimyt990501 <kimyt990501@naver.com>
2024-02-11 13:41:47 +0900

    크롤링 코드 수정

--- data_crawling.py
+++ data_crawling.py
@@ -35,30 +35,36 @@ for i in range(1, 21):
     
     # 1군일 경우
     try:
-        imgUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_playerProfile_imgProgile"]').get_attribute("src")
+        #imgUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_playerProfile_imgProgile"]').get_attribute("src")
         nameUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_playerProfile_lblName"]')
         birthUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_playerProfile_lblBirthday"]')
+        leftrightposUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_playerProfile_lblPosition"]')
+        heightUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_playerProfile_lblHeightWeight"]')
     # 퓨쳐스리그인 경우
     except NoSuchElementException:
-        imgUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_ucPlayerProfile_imgProfile"]').get_attribute("src")
+        #imgUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_ucPlayerProfile_imgProfile"]').get_attribute("src")
         nameUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_ucPlayerProfile_lblName"]')
         birthUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_ucPlayerProfile_lblBirthday"]')
+        leftrightposUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_ucPlayerProfile_lblPosition"]')
+        heightUrl = driver.find_element(By.XPATH, '//*[@id="cphContents_cphContents_cphContents_ucPlayerProfile_lblHeightWeight"]')
     
     name = nameUrl.text # 이름
     year = birthUrl.text[:4] # 년
     month = birthUrl.text[6:8] # 월
     day = birthUrl.text[10:12] # 일
+    position = leftrightposUrl.text[:-6] # 포지션
+    height = heightUrl.text[:3] # 키
     birth = year + month + day
-    temp = [name, birth]
-    list.append(temp) # 리스트에 이름, 생년월일 저장
+    temp = [name, birth, position, height]
+    list.append(temp) # 리스트에 정보 저장
     
     # 이미지 저장
-    urllib.request.urlretrieve(imgUrl, "data/samsung/" + birth + ".png")
+    #urllib.request.urlretrieve(imgUrl, "data/samsung/" + birth + ".png")
 
     driver.back() # 뒤로가기
 '''
 # 선수 정보 데이터프레임에 저장 후 csv 파일 추출
 df = pd.DataFrame(list)
-df.columns = ['Name', 'Birth']
+df.columns = ['name', 'birth', 'position', 'height']
 df.to_csv('data/player_info.csv', encoding='euc-kr')
 '''
