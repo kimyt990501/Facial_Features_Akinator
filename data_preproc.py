e0d8c94b081e2af5b2cac9ddf05f2c37c7b3d25b
kimyt990501 <kimyt990501@naver.com>
2024-02-11 14:13:20 +0900

    데이터 전처리

--- data_preproc.py
+++ data_preproc.py
@@ -5,22 +5,14 @@ warnings.filterwarnings('ignore')
 
 info = pd.read_csv('C:\\Users\\user\\Desktop\\github_repo\\Facial_Features_Akinator\\data\\player_info.csv', encoding='euc-kr', index_col=False)
 info = info.drop(['Unnamed: 0'], axis=1)
-#info.columns = ['name', 'birth']
 #print(info)
 
 samsung = pd.read_csv('C:\\Users\\user\\Desktop\\github_repo\\Facial_Features_Akinator\\data\\samsung_face.csv', index_col=False)
 samsung = samsung.drop(['Unnamed: 0'], axis=1)
 #print(samsung)
 
-'''
-info_sub = pd.read_csv('C:\\Users\\user\\Desktop\\imgtest\\data\\player_info_sub.csv', encoding='euc-kr', index_col=False)
-info_sub = info_sub.drop(['Unnamed: 0'], axis=1)
-info_sub.columns = ['birth', 'position', 'height']
-'''
-
 result = pd.merge(info, samsung, on='birth', how='inner')
-#result = pd.merge(result, info_sub, on='birth', how='inner')
-print(result)
+#print(result)
 
 # 포지션 판별 칼럼 추가
 result['catcher'] = result['position'].apply(lambda x: 1 if x == '포수' else 0)
