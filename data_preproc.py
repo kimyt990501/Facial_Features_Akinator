bba390e4c57f21f86cb610557c55da7c82414037
kimyt990501 <kimyt990501@naver.com>
2024-02-11 13:53:43 +0900

    데이터 전처리

--- data_preproc.py
+++ data_preproc.py
@@ -3,12 +3,12 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
-info = pd.read_csv('C:\\Users\\user\\Desktop\\imgtest\\data\\player_info.csv', encoding='euc-kr', index_col=False)
+info = pd.read_csv('C:\\Users\\user\\Desktop\\github_repo\\Facial_Features_Akinator\\data\\player_info.csv', encoding='euc-kr', index_col=False)
 info = info.drop(['Unnamed: 0'], axis=1)
 #info.columns = ['name', 'birth']
 #print(info)
 
-samsung = pd.read_csv('C:\\Users\\user\\Desktop\\imgtest\\data\\samsung_face.csv', index_col=False)
+samsung = pd.read_csv('C:\\Users\\user\\Desktop\\github_repo\\Facial_Features_Akinator\\data\\samsung_face.csv', index_col=False)
 samsung = samsung.drop(['Unnamed: 0'], axis=1)
 #print(samsung)
 
@@ -29,4 +29,4 @@ result['outfielder'] = result['position'].apply(lambda x: 1 if x == '외야수' 
 result['infielder'] = result['position'].apply(lambda x: 1 if x == '내야수' else 0)
 result = result.drop(['position'], axis=1)
 
-result.to_csv('C:\\Users\\user\\Desktop\\imgtest\\data\\samsung_facialfeatures.csv', encoding='euc-kr', index=False)
+result.to_csv('C:\\Users\\user\\Desktop\\github_repo\\Facial_Features_Akinator\\data\\samsung_facialfeatures.csv', encoding='euc-kr', index=False)
